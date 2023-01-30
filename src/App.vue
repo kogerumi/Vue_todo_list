194a1efb74fcc37ffd6065e1ffbae7d594d50be1
Malorodova <LAbsente@yandex.ru>
2023-01-31 01:02:33 +0300

    Inicial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <div class="container">
+        <input type="text" name="todo_input" placeholder="Write a task" v-model="taskText">
+        <button @click="addTask">Add task</button>
+        <p>{{tasks}}</p>
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+
+    },
+    data() {
+        return {
+            taskText: "",
+            tasks: []
+        }
+    },
+    methods: {
+        addTask() {
+            this.tasks.push(this.taskText);
+            this.taskText = "";
+        }
+    }
+
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
